b8c4b562076ad27c9170830794aaf9d04599058b
tianxiaom <tianxiao.ma@citrix.com>
2016-10-10 13:30:42 +0800

    move podio config to .env

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,8 +1,14 @@
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const AUTH_TYPE = 'client';
-export const CLIENT_ID = IS_DEV ? 'test-7k47ez' : '';
+
+export const CLIENT_ID = IS_DEV ?
+  'localhost-test-7epwwz' : process.env.REACT_APP_CLIENT_ID;
+
 export const CLIENT_SECRET = IS_DEV ?
-  '9TibtoG2vFrfoZPVNcFUsTw8dCul8qgEJdedpJbtJn2qooMyvs4qdUOjrT2qXfSk' : '';
+  'Ow3wd80FBYbxODnOrQk0onfXRqyJ0CsQI4bbUE8Bs8UANLbrRShDKJpM8Qp5HzTx' :
+  process.env.REACT_APP_CLIENT_SECRET;
+
 export const REDIRECT_URL = IS_DEV ?
-  'http://10.157.14.136:3000/auth-popup' : '';
+  'http://localhost:3000/auth-popup' :
+  `http://${process.env.REACT_APP_HOST}/auth-popup`;
